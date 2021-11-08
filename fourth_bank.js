b095b429676b1f2b00f9ffd6a27be11859972380
aximus4206 <aximus4206@gmail.com>
2021-11-08 13:09:22 +0200

    dom

---
+++ fourth_bank.js
@@ -0,0 +1,242 @@
+class BankView {
+    constructor(data) {
+        this.data = data;
+        this.information = document.querySelector('.information');
+        this.buttonWindowOpen = document.querySelector('.createButton');
+        this.cardItem = document.querySelector('.cards__item');
+        this.modal = document.querySelector('.modal');
+        this.modalWindow = document.querySelector('.modal__window');
+        this.modalValue = null;
+        this.aboutBank();
+        this.showClient();
+    }
+
+    aboutBank() {
+        let totalBalance = bankOschad.countTotalBalanceBank();
+        let creditMoney = bankOschad.countCreditMoney();
+        let debtorsTotalDebts = bankOschad.countLoanDebtorsTotalDebt();
+
+        const totalBankBalance = document.createElement('div');
+        totalBankBalance.className = 'totalBankBalance';
+
+        totalBalance.then(result => {
+            totalBankBalance.innerHTML = `
+            <p class="info"> Bank current balance : ${result} UAH</p>
+            `;
+        });
+
+        const totalCreditMoney = document.createElement('div');
+        totalCreditMoney.className = 'totalCreditMoney';
+
+        creditMoney.then(result => {
+            totalCreditMoney.innerHTML = `
+            <p class="info"> Current credit money : ${result} UAH</p>
+            `;
+        });
+
+        const debtorsDebt = document.createElement('div');
+        debtorsDebt.className = 'debtorsDebt';
+
+        debtorsTotalDebts.then(result => {
+            debtorsDebt.innerHTML = `
+            <p class="info"> Total debt : ${result} UAH</p>
+            `;
+        });
+
+        this.information.append(totalBankBalance);
+        this.information.append(debtorsDebt);
+        this.information.append(totalCreditMoney);
+    }
+
+    showClient() {
+        this.cardItem.innerHTML = ' ';
+        const cards = this.data.map((data, index) => this.showClients(data, index));
+        this.cardItem.append(...cards);
+    }
+
+    showClients(data, index) {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+        <h3 class="client-fullname"> Fullname: <span class="fullname">${data.name} ${data.surname}</span></h3>
+        <p class="client-info">Balance: ${data.credit.ownBalance.ownBalance} ${data.credit.ownBalance.currency}</p>
+        <p class="client-info">Credit Balance: ${data.credit.creditBalance.creditBalance} ${data.credit.creditBalance.currency}</p>
+        <p class="client-info">Credit Limit: ${data.credit.creditBalance.creditLimit} ${data.credit.creditBalance.currency}</p>
+        <p class="client-info">Debit Balance: ${data.debit.curentBalance} ${data.debit.currency}</p>
+        <p class="client-info">Activeted date: ${data.credit.ownBalance.activetedDate}</p>
+        <p class="client-info">Expired date: ${data.credit.ownBalance.expiredDate}</p>
+        <p class="client-info">Client status: ${data.isActive ? 'active' : 'inactive'}</p>
+        `;
+
+        const cardButtonContainer = document.createElement('div');
+        cardButtonContainer.className = 'cardButtonContainer';
+        card.append(cardButtonContainer);
+
+        const modifyButton = document.createElement('button');
+        modifyButton.className = 'button';
+        modifyButton.innerHTML = 'Modify';
+        cardButtonContainer.appendChild(modifyButton);
+        modifyButton.addEventListener('click', this.modifyWindow.bind(this, data, index));
+
+        this.buttonWindowOpen.addEventListener('click', this.createWindow.bind(this, data));
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'button';
+        deleteButton.innerHTML = 'Delete';
+        cardButtonContainer.appendChild(deleteButton);
+        deleteButton.addEventListener('click', this.deleteClient.bind(this, index));
+
+        return card;
+    }
+
+    modalDisplay(data, index) {
+        this.modal.classList.add('active');
+        this.modalWindow.innerHTML = `
+        <h2>Operate window</h2>
+        <div class="modal-item">
+            <p> Name </p>
+            <input class="input" type="text" placeholder="Type client name" name="name"> 
+        </div>
+        <div class="modal-item">
+            <p> Surname </p>
+            <input class="input" type="text" placeholder="Type client surname " name="surname"> 
+        </div>
+        <div class="modal-item">
+            <p> Debit Balance </p>
+            <input class="input" type="text" placeholder="Type client Debit Balance" name="curentBalance"> 
+        </div>
+        <div class="modal-item">
+            <p> Activeted Date </p>
+            <input class="input" type="text" placeholder="Type client balance Activeted Date " name="activetedDate"> 
+        </div>
+        <div class="modal-item">
+            <p> Expired Date </p>
+            <input class="input" type="text" placeholder="Type client balance Expired Date " name="expiredDate"> 
+        </div>
+        <div class="modal-item">
+            <p> Own Balance </p>
+            <input class="input" type="text" placeholder="Type client Balance " name="ownBalance"> 
+        </div>
+        <div class="modal-item">
+            <p> Сredit Balance </p>
+            <input class="input" type="text" placeholder="Type client Сredit Balance " name="creditBalance"> 
+        </div>
+        <div class="modal-item">
+            <p> Сredit Limit </p>
+            <input class="input" type="text" placeholder="Type client Сredit Limit " name="creditLimit"> 
+        </div>
+        <div class="modal-item">
+            <p> Currency </p>
+            <input class="input" type="text" placeholder="Type client Currency " name="currency"> 
+        </div>
+        <div class="modal-check">
+            <p> Status (active/inactive) </p>
+            <input class="check" type="checkbox" placeholder="Type Client status " name="isActive"> 
+        </div>
+        `;
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.className = 'buttonContainer';
+        this.modalWindow.append(buttonContainer);
+
+        const confirmButton = document.createElement('div');
+        confirmButton.className = 'confirmButton';
+        buttonContainer.appendChild(confirmButton);
+
+        const button = document.createElement('button');
+        button.className = 'createNewClient';
+        button.innerText = 'Confirm';
+        confirmButton.appendChild(button);
+
+        confirmButton.addEventListener('click', this.createNewClient.bind(this, index));
+
+        const closeDiv = document.createElement('div');
+        closeDiv.className = 'closeDiv';
+        buttonContainer.appendChild(closeDiv);
+
+        const buttonClose = document.createElement('button');
+        buttonClose.className = 'closeButton';
+        buttonClose.innerText = 'Close';
+        closeDiv.appendChild(buttonClose);
+
+        closeDiv.addEventListener('click', this.closeWindow.bind(this));
+    }
+
+    createWindow(data) {
+        this.modalValue = false;
+        this.modalDisplay(data);
+    }
+
+    modifyWindow(data, index) {
+        this.modalValue = true;
+        this.modalDisplay(data, index);
+    }
+
+    closeWindow() {
+        this.modal.classList.remove('active');
+    }
+
+    createClient(event) {
+        event.preventDefault();
+
+        let newClient = new FormData(event.target.closest('form'));
+
+        this.data.unshift({
+            name: newClient.get('name') || 'Name',
+            surname: newClient.get('surname') || 'Surname',
+            isActive: newClient.get('isActive') || false,
+            debit: {
+                curentBalance: Number(newClient.get('debitBalance')) || 0,
+                activetedDate: newClient.get('activetedDate') || 'Activeted Date',
+                expiredDate: newClient.get('expiredDate') || 'Expired Date',
+                currency: newClient.get('currency') || 'UAH',
+            },
+            credit: {
+                ownBalance: {
+                    ownBalance: Number(newClient.get('ownBalance')) || 0,
+                    currency: newClient.get('currency') || 'UAH',
+                },
+                creditBalance: {
+                    creditBalance: Number(newClient.get('creditBalance')) || 0,
+                    creditLimit: Number(newClient.get('creditLimit')) || 0,
+                    currency: newClient.get('currency') || 'UAH',
+                },
+            },
+        });
+        this.showClient();
+    }
+
+    createNewClient(index, event) {
+        this.information.innerHTML = ' ';
+        if (this.modalValue === true) {
+            this.modifyClient(event, index);
+        } else if (this.modalValue === false) {
+            this.createClient(event);
+        }
+        this.closeWindow();
+        this.aboutBank();
+    }
+
+    deleteClient(index) {
+        this.data.splice(index, 1);
+        this.showClient();
+    }
+
+    modifyClient(event, i) {
+        event.preventDefault();
+
+        let workerModify = new FormData(event.target.closest('form'));
+
+        this.data[i].name = workerModify.get('name') || this.data[i].name;
+        this.data[i].surname = workerModify.get('surname') || this.data[i].surname;
+        this.data[i].credit.ownBalance.ownBalance = workerModify.get('balance') || this.data[i].credit.ownBalance.ownBalance;
+        this.data[i].credit.ownBalance.activetedDate = workerModify.get('activetedDate') || this.data[i].credit.ownBalance.activetedDate;
+        this.data[i].credit.ownBalance.expiredDate = workerModify.get('expiredDate') || this.data[i].credit.ownBalance.expiredDate;
+        this.data[i].credit.creditBalance.creditBalance = workerModify.get('creditBalance') || this.data[i].credit.creditBalance.creditBalance;
+        this.data[i].credit.creditBalance.creditLimit = workerModify.get('creditLimit') || this.data[i].credit.creditBalance.creditLimit;
+        this.data[i].isActive = workerModify.get('isActive') || this.data[i].isActive;
+
+        this.showClient();
+    }
+}
+const theBankView = new BankView(bankClients);
---
+++ fourth_rest.js
@@ -0,0 +1,290 @@
+
+class RestView {
+    constructor(data) {
+        this.data = data;
+        this.information = document.querySelector('.information');
+        this.information.leftCol = document.querySelector('.left-col');
+        this.information.rightCol = document.querySelector('.right-col');
+        this.cardItem = document.querySelector('.cards__item');
+        this.buttonWindowOpen = document.querySelector('.createButton');
+        this.modal = document.querySelector('.modal');
+        this.modalWindow = document.querySelector('.modal__window');
+        this.modalValue = null;
+        this.aboutRestoraunt();
+        this.showWorker();
+    }
+
+    aboutRestoraunt() {
+        let departmentsSalary = rest.countWholeSalaryDepartment(false);
+        let middleDepartmentsSalary = rest.middleDepartmentSalary(false);
+        let minDepartmentsSalary = rest.minDepartmentSalary(false);
+        let maxDepartmentsSalary = rest.maxDepartmentSalary(false);
+        let minPositionsSalary = rest.minPositionSalary(false);
+        let maxPositionsSalary = rest.maxPositionSalary(false);
+        let withoutDeputy = rest.showDepartmentWithoutPosition('deputyhead');
+        let areDismissed = rest.dismissedWorkers(true);
+
+        const departmentsSalaryInfo = document.createElement('div');
+        departmentsSalaryInfo.className = 'departmentsSalary';
+
+        const departmentSalaryHeader = document.createElement('h3');
+        departmentSalaryHeader.innerHTML = '<h3>Total salary of department:</h3>';
+
+        const middleDepartmentsSalaryInfo = document.createElement('div');
+        middleDepartmentsSalaryInfo.className = 'departmentsMiddleSalary';
+
+        const middleDepartmentsSalaryHeader = document.createElement('h3');
+        middleDepartmentsSalaryHeader.innerHTML = '<h3>Middle salary of department:</h3>';
+
+        const minDepartmentsSalaryInfo = document.createElement('div');
+        minDepartmentsSalaryInfo.className = 'departmentsMinSalary';
+
+        const minDepartmentsSalaryHeader = document.createElement('h3');
+        minDepartmentsSalaryHeader.innerHTML = '<h3>Smallest salary of department:</h3>';
+
+        const maxDepartmentsSalaryInfo = document.createElement('div');
+        maxDepartmentsSalaryInfo.className = 'departmentsMaxSalary';
+
+        const maxDepartmentsSalaryHeader = document.createElement('h3');
+        maxDepartmentsSalaryHeader.innerHTML = '<h3>Highest salary of department:</h3>';
+
+        const minPositionsSalaryInfo = document.createElement('div');
+        minPositionsSalaryInfo.className = 'positionsMinSalary';
+
+        const minPositionsSalaryHeader = document.createElement('h3');
+        minPositionsSalaryHeader.innerHTML = '<h3>Smallest salary of each position:</h3>';
+
+        const maxPositionsSalaryInfo = document.createElement('div');
+        maxPositionsSalaryInfo.className = 'positionsMaxSalary';
+
+        const maxPositionsSalaryHeader = document.createElement('h3');
+        maxPositionsSalaryHeader.innerHTML = '<h3>Highest salary of each position:</h3>';
+
+        const departmentsWithoutDeputyInfo = document.createElement('div');
+        departmentsWithoutDeputyInfo.className = 'departmentWithoutDeputy';
+
+        const departmentsWithoutDeputyHeader = document.createElement('h3');
+        departmentsWithoutDeputyHeader.innerHTML = '<h3>Department without deputy:</h3>';
+
+        const dismissedAmountInfo = document.createElement('div');
+        dismissedAmountInfo.className = 'dismissedAmount';
+
+        const dismissedAmountHeader = document.createElement('h3');
+        dismissedAmountHeader.innerHTML = '<h3>Dismissed in department:</h3>';
+
+        for (let value in departmentsSalary) {
+            let result = value;
+            departmentsSalaryInfo.innerHTML += `<p class="info"> ${++result} department: ${departmentsSalary[value]} $</p>`;
+        }
+
+        for (let value in middleDepartmentsSalary) {
+            let result = value;
+            middleDepartmentsSalaryInfo.innerHTML += `<p class="info"> ${++result} department: ${middleDepartmentsSalary[value]} $</p>`;
+        }
+
+        for (let value in minDepartmentsSalary) {
+            minDepartmentsSalaryInfo.innerHTML += `<p class="info"> ${value}: ${minDepartmentsSalary[value]} $</p>`;
+        }
+
+        for (let value in maxDepartmentsSalary) {
+            maxDepartmentsSalaryInfo.innerHTML += `<p class="info"> ${value}: ${maxDepartmentsSalary[value]} $</p>`;
+        }
+
+        for (let value in minPositionsSalary) {
+            minPositionsSalaryInfo.innerHTML += `<p class="info"> ${value}: ${minPositionsSalary[value]} $</p>`;
+        }
+
+        for (let value in maxPositionsSalary) {
+            maxPositionsSalaryInfo.innerHTML += `<p class="info"> ${value}: ${maxPositionsSalary[value]} $</p>`;
+        }
+
+        for (let value in withoutDeputy) {
+            departmentsWithoutDeputyInfo.innerHTML += `<p class="info"> ${withoutDeputy[value]} </p>`;
+        }
+
+        for (let value in areDismissed) {
+            let result = value;
+            dismissedAmountInfo.innerHTML += `<p class="info"> ${++result} department: ${areDismissed[value]} worker(s)</p>`;
+        }
+
+        this.information.leftCol.append(departmentSalaryHeader);
+        this.information.leftCol.append(departmentsSalaryInfo);
+        this.information.leftCol.append(middleDepartmentsSalaryHeader);
+        this.information.leftCol.append(middleDepartmentsSalaryInfo);
+        this.information.leftCol.append(minDepartmentsSalaryHeader);
+        this.information.leftCol.append(minDepartmentsSalaryInfo);
+        this.information.leftCol.append(maxDepartmentsSalaryHeader);
+        this.information.leftCol.append(maxDepartmentsSalaryInfo);
+        this.information.rightCol.append(minPositionsSalaryHeader);
+        this.information.rightCol.append(minPositionsSalaryInfo);
+        this.information.rightCol.append(maxPositionsSalaryHeader);
+        this.information.rightCol.append(maxPositionsSalaryInfo);
+        this.information.rightCol.append(departmentsWithoutDeputyHeader);
+        this.information.rightCol.append(departmentsWithoutDeputyInfo);
+        this.information.rightCol.append(dismissedAmountHeader);
+        this.information.rightCol.append(dismissedAmountInfo);
+    }
+
+    showWorker() {
+        this.cardItem.innerHTML = ' ';
+        const cards = this.data.map((data, index) => this.showWorkers(data, index));
+        this.cardItem.append(...cards);
+    }
+
+    showWorkers(data, index) {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+        <h3 class="fullname">fullname: <span class="fullname-user">${data.name} ${data.surname}</span></h3>
+        <div class="card-container">
+        <p class="personal-info">department: ${data.department}</p>
+        <p class="personal-info">position: ${data.position}</p>
+        </div>
+        <div class="card-container">
+        <p class="personal-info">salary: ${data.salary}$</p>
+        <p class="personal-info">dismissed: ${data.dismissed ? 'dismissed' : 'active'}</p>
+        </div>
+        `;
+
+        const cardButtonContainer = document.createElement('div');
+        cardButtonContainer.className = 'cardButtonContainer';
+        card.append(cardButtonContainer);
+
+        const modifyButton = document.createElement('button');
+        modifyButton.className = 'button';
+        modifyButton.innerHTML = 'Modify';
+        cardButtonContainer.appendChild(modifyButton);
+        modifyButton.addEventListener('click', this.modifyWindow.bind(this, data, index));
+
+        this.buttonWindowOpen.addEventListener('click', this.createWindow.bind(this, data));
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'button';
+        deleteButton.innerHTML = 'Delete';
+        cardButtonContainer.appendChild(deleteButton);
+        deleteButton.addEventListener('click', this.deleteWorker.bind(this, index));
+
+        return card;
+    }
+
+    modalDisplay(data, index) {
+        this.modal.classList.add('active');
+        this.modalWindow.innerHTML = `
+        <h2>Operate window</h2>
+        <div class="modal-item">
+            <p> Name </p>
+            <input class="modal-input" type="text" placeholder="Type worker name" name="name"> 
+        </div>
+        <div class="modal-item">
+            <p> Surname </p>
+            <input class="modal-input" type="text" placeholder="Type worker surname" name="surname"> 
+        </div>
+        <div class="modal-item">
+            <p> Department </p>
+            <input class="modal-input" type="text" placeholder="Type worker department" name="department"> 
+        </div>
+        <div class="modal-item">
+            <p> Position </p>
+            <input class="modal-input" type="text" placeholder="Type worker position" name="position"> 
+        </div>
+        <div class="modal-item">
+            <p> Salary </p>
+            <input class="modal-input" type="text" placeholder="Type worker salary" name="salary"> 
+        </div>
+        <div class="modal-check">
+            <p> Dismissed </p>
+            <input class="choose" type="checkbox" placeholder="Choose dismissed or active" name="dismissed"> 
+        </div>
+        `;
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.className = 'buttonContainer';
+        this.modalWindow.append(buttonContainer);
+
+        const confirmButton = document.createElement('div');
+        confirmButton.className = 'confirmButton';
+        buttonContainer.appendChild(confirmButton);
+
+        const button = document.createElement('button');
+        button.className = 'createNewWorker';
+        button.innerText = 'Confirm';
+        confirmButton.appendChild(button);
+
+        confirmButton.addEventListener('click', this.createNewWorker.bind(this, index));
+
+        const closeDiv = document.createElement('div');
+        closeDiv.className = 'closeDiv';
+        buttonContainer.appendChild(closeDiv);
+
+        const buttonClose = document.createElement('button');
+        buttonClose.className = 'closeButton';
+        buttonClose.innerText = 'Close';
+        closeDiv.appendChild(buttonClose);
+
+        closeDiv.addEventListener('click', this.closeWindow.bind(this));
+    }
+
+    createWindow(data) {
+        this.modalValue = false;
+        this.modalDisplay(data);
+    }
+
+    closeWindow() {
+        this.modal.classList.remove('active');
+    }
+
+    modifyWindow(data, index) {
+        this.modalValue = true;
+        this.modalDisplay(data, index);
+    }
+
+    createWorker(event) {
+        event.preventDefault();
+
+        let newWorker = new FormData(event.target.closest('form'));
+
+        this.data.unshift({
+            name: newWorker.get('name') || 'Name',
+            surname: newWorker.get('surname') || 'Surname',
+            department: Number(newWorker.get('department')) || 'Department',
+            position: newWorker.get('position') || 'Position',
+            salary: Number(newWorker.get('salary')) || 'Salary',
+            dismissed: newWorker.get('dismissed') || false,
+        });
+        this.showWorker();
+    }
+
+    createNewWorker(index, event) {
+        this.information.leftCol.innerHTML = ' ';
+        this.information.rightCol.innerHTML = ' ';
+        if (this.modalValue === true) {
+            this.modifyWorker(event, index);
+        } else if (this.modalValue === false) {
+            this.createWorker(event);
+        }
+        this.closeWindow();
+        this.aboutRestoraunt();
+    }
+
+    deleteWorker(index) {
+        this.data.splice(index, 1);
+        this.showWorker();
+    }
+
+    modifyWorker(event, i) {
+        event.preventDefault();
+
+        let workerModify = new FormData(event.target.closest('form'));
+
+        this.data[i].name = workerModify.get('name') || this.data[i].name;
+        this.data[i].surname = workerModify.get('surname') || this.data[i].surname;
+        this.data[i].department = workerModify.get('department') || this.data[i].department;
+        this.data[i].position = workerModify.get('position') || this.data[i].position;
+        this.data[i].salary = workerModify.get('salary') || this.data[i].salary;
+        this.data[i].dismissed = workerModify.get('dismissed') || this.data[i].dismissed;
+
+        this.showWorker();
+    }
+
+}
+const restorauntView = new RestView(workersArr);
